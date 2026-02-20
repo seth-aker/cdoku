c5783461e6277aa16a69a100184369b1053a306f
Seth Aker <136737929+seth-aker@users.noreply.github.com>
2026-02-20 00:57:12 -0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/saker/workspace/cdoku
-# Build directory: /home/saker/workspace/cdoku/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
