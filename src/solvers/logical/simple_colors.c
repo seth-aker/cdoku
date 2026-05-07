4f3644827785a6c76f894a6ba21887bc3034f809
Seth Aker <saker132@gmail.com>
2026-05-06 23:53:17 -0400

    Adding simple colors work

--- src/solvers/logical/simple_colors.c
+++ src/solvers/logical/simple_colors.c
@@ -7,7 +7,13 @@ TechniqueResult find_simple_color(Puzzle* puzzle) {
     context.pairs_count = 0;
     collect_chain_pairs(puzzle, &context, i);
 
-    if(context.pairs_count > 2)
+    if(context.pairs_count < 2) {
+      continue;
+    }
+    for(int p = 0; p < context.pairs_count; ++p) {
+
+
+    }
   }
 }
 
@@ -17,19 +23,34 @@ void collect_chain_pairs(const Puzzle* puzzle, SimpleColorContext* context, int 
     .mask = 1 << (candidate - 1),
     .max_count = 2
   };
+  int pair_idxs[2];
   for(int i = 0; i < PUZZLE_WIDTH; ++i) {
     const uint8_t* house_idxs = ROW_TO_IDXS[i];
-    int cand_count = get_candidate_positions(puzzle, house_idxs, search_params, context->pairs[context->pairs_count].pairIdxs);
+    int cand_count = get_candidate_positions(puzzle, house_idxs, search_params, pair_idxs);
     if(cand_count == 2) {
-      context->pairs[context->pairs_count++].type = ROW;
+      int pair_count = context->pairs_count;
+      ChainCell* cell_one = &context->pairs[pair_count].pair[0];
+      ChainCell* cell_two = &context->pairs[pair_count].pair[1];
+      cell_one->idx = pair_idxs[0];
+      cell_two->idx = pair_idxs[1];
+      cell_one->row = i;
+      cell_two->row = i;
+      cell_one->col = IDX_TO_COL[pair_idxs[0]];
+      cell_two->col = IDX_TO_COL[pair_idxs[1]];
+      // add the cells to the row set
+      context->rowCells[i].cells[context->rowCells[i].count++] = &cell_one;
+      context->rowCells[i].cells[context->rowCells[i].count++] = &cell_two;
+      // add the cells to the col set
+
+      context->pairs_count++;
     }
     house_idxs = COL_TO_IDXS[i];
-    cand_count = get_candidate_positions(puzzle, house_idxs, search_params, context->pairs[context->pairs_count].pairIdxs);
+    cand_count = get_candidate_positions(puzzle, house_idxs, search_params, pair_idxs);
     if(cand_count == 2) {
       context->pairs[context->pairs_count++].type = COL;
     }
     house_idxs = BLOCK_TO_IDXS[i];
-    cand_count = get_candidate_positions(puzzle, house_idxs, search_params, context->pairs[context->pairs_count].pairIdxs);
+    cand_count = get_candidate_positions(puzzle, house_idxs, search_params, pair_idxs);
     if(cand_count == 2) {
       context->pairs[context->pairs_count++].type = BLOCK;
     }
--- src/solvers/logical/simple_colors.h
+++ src/solvers/logical/simple_colors.h
@@ -7,13 +7,35 @@ typedef enum HouseType {
   BOX
 } HouseType;
 
+typedef enum Color {
+  DARK,
+  LIGHT
+} Color;
+typedef struct ChainHouse {
+  ChainCell* cells[9];
+  int count;
+} ChainHouse;
+
+typedef struct ChainCell {
+  int idx;
+  int row;
+  int col;
+  Color color;
+} ChainCell;
+
 typedef struct ChainPair {
-  int pairIdxs[2];
+  ChainCell pair[2];
   HouseType type;
 } ChainPair;
 
 typedef struct SimpleColorContext {
   ChainPair pairs[27];
+  ChainHouse rowCells[9];
+  ChainHouse colCells[9];
+  ChainHouse blockCells[9];
   int pairs_count;
 } SimpleColorContext;
+
+// start at a node go both ways in the chain marking the colors -> follow the chain to the end 
+// check if any of them conflict
 #endif
