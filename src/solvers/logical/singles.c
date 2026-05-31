24897b2a218e131584626bb9ca9170b814defa28
Seth Aker <saker132@gmail.com>
2026-05-31 19:54:33 -0400

    Muting some error messages to debug level

--- src/solvers/logical/singles.c
+++ src/solvers/logical/singles.c
@@ -128,8 +128,10 @@ TechniqueResult find_hidden_single(Puzzle* puzzle) {
     unique = candidate_mask & ~other_cell_masks;
     if(unique) {
       if(__builtin_popcount(unique) != 1) {
-        log_error("Error: Found multiple unique candiates in cell idx: %d", i);
+        log_debug("Error: Found multiple unique candiates in cell idx: %d", i);
+#ifndef NDEBUG
         print_puzzle_state(puzzle);
+#endif
         return INVALID_STATE;
       }
       int found_single = __builtin_ctz(unique) + 1;
@@ -157,8 +159,10 @@ TechniqueResult find_hidden_single(Puzzle* puzzle) {
     unique = candidate_mask & ~other_cell_masks;
     if(unique) {
       if(__builtin_popcount(unique) != 1) {
-        log_error("Error: Found multiple unique candiates in cell idx: %d", i);
+        log_debug("Error: Found multiple unique candiates in cell idx: %d", i);
+#ifndef NDEBUG
         print_puzzle_state(puzzle);
+#endif
         return INVALID_STATE;
       }
       int found_single = __builtin_ctz(unique) + 1;
